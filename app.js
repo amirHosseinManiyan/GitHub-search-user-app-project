26678b28aa5e0ade26fab5629eea7eb22487e1d2
AmirHosseinManiyan <amirhosseinmaniyan@gmail.com>
2026-09-08 17:07:55 +0330

    build mobile layout and add theme persistence

---
+++ app.js
@@ -0,0 +1,33 @@
+const html = document.documentElement;
+const themeIcon = document.querySelector(".theme-icon");
+const themeLabel = document.querySelector(".theme-label");
+let currentTheme = html.dataset.theme;
+
+function changeTheme(newTheme) {
+   html.dataset.theme = newTheme;
+   currentTheme = newTheme;
+   if (newTheme === "dark") {
+      themeIcon.setAttribute("src", "assets/icon-sun.svg");
+      themeLabel.textContent = "LIGHT";
+   } else if (newTheme === "light") {
+      themeIcon.setAttribute("src", "assets/icon-moon.svg");
+      themeLabel.textContent = "DARK"
+   }
+}
+
+let savedTheme = localStorage.getItem("theme");
+if (savedTheme === "light" || savedTheme === null) {
+   changeTheme("light");
+} else if (savedTheme === "dark") {
+   changeTheme("dark");
+}
+
+themeIcon.addEventListener("click", function() {
+   if (currentTheme === "light") {
+      changeTheme("dark");
+      localStorage.setItem("theme", "dark");
+   } else if (currentTheme === "dark") {
+      changeTheme("light");
+      localStorage.setItem("theme", "light");
+   }
+});
